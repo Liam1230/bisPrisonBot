45a9a17df18eb50e3120f1beb7ce44b72962375b
Liam1230 <Kin0k0daichi>
2020-06-14 16:45:33 +0900

    レイアウトの修正

--- pages/index.vue
+++ pages/index.vue
@@ -67,72 +67,80 @@
 			</h3>
 			<v-container>
 				<v-row>
-					<v-checkbox 
-						v-model="checkboxItems" 
-						label="SNS" 
-						color="red" 
-						value="0" 
-						hide-details
-						class="pl-3 ml-3"
-						true-value="1"
-						false-value="0"
-					></v-checkbox>
-				
-					<v-checkbox 
-						v-model="checkboxItems" 
-						label="Web" 
-						color="red darken-3" 
-						value="1" 
-						hide-details
-						class="pl-3 ml-3"
-						true-value="1"
-						false-value="0"
-					></v-checkbox>
-				
-					<v-checkbox
-						v-model="checkboxItems"
-						label="知り合い"
-						color="indigo"
-						value="2"
-						hide-details
-						class="pl-3 ml-3"
-						true-value="1"
-						false-value="0"
-					></v-checkbox>
+					<v-col cols="4" md="4" style="padding:0px">
+						<v-checkbox
+							v-model="checkboxItems" 
+							label="SNS" 
+							color="red" 
+							value="0" 
+							hide-details
+							class="pl-3 ml-3 text-caption"
+							true-value="1"
+							false-value="0"
+						></v-checkbox>
+					</v-col>
+					<v-col cols="4" md="4" style="padding:0px">
+						<v-checkbox 
+							v-model="checkboxItems" 
+							label="Web" 
+							color="red darken-3" 
+							value="1" 
+							hide-details
+							class="pl-3 ml-3"
+							true-value="1"
+							false-value="0"
+						></v-checkbox>
+					</v-col>
+					<v-col cols="4" md="4" style="padding:0px">
+						<v-checkbox
+							v-model="checkboxItems"
+							label="知り合い"
+							color="indigo"
+							value="2"
+							hide-details
+							class="pl-3 ml-3"
+							true-value="1"
+							false-value="0"
+						></v-checkbox>
+					</v-col>
 				</v-row>	
 				<v-row>
-					<v-checkbox
-						v-model="checkboxItems"
-						label="紹介"
-						color="indigo darken-3"
-						value="3"
-						hide-details
-						class="pl-3 ml-3"
-						true-value="1"
-						false-value="0"
-					></v-checkbox>
-			
-					<v-checkbox
-						v-model="checkboxItems"
-						label="交流会"
-						color="orange"
-						value="4"
-						hide-details
-						class="pl-3 ml-3"
-						true-value="1"
-						false-value="0"
-					></v-checkbox>
-
-					<v-checkbox
-						v-model="checkboxItems"
-						label="セミナー"
-						color="orange darken-3"
-						value="5"
-						hide-details
-						class="pl-3 ml-3"
-						true-value="1"
-						false-value="0"
-					></v-checkbox>
+					<v-col cols="4" md="4" style="padding:0px">
+						<v-checkbox
+							v-model="checkboxItems"
+							label="紹介"
+							color="indigo darken-3"
+							value="3"
+							hide-details
+							class="pl-3 ml-3"
+							true-value="1"
+							false-value="0"
+						></v-checkbox>
+					</v-col>
+					<v-col cols="4" md="4" style="padding:0px">
+						<v-checkbox
+							v-model="checkboxItems"
+							label="交流会"
+							color="orange"
+							value="4"
+							hide-details
+							class="pl-3 ml-3"
+							true-value="1"
+							false-value="0"
+						></v-checkbox>
+					</v-col>
+					<v-col cols="4" md="4" style="padding:0px;">
+						<v-checkbox
+							v-model="checkboxItems"
+							label="セミナー"
+							color="orange darken-3"
+							value="5"
+							hide-details
+							class="pl-3 ml-3"
+							true-value="1"
+							false-value="0"
+						></v-checkbox>
+					</v-col>
 				</v-row>
 			</v-container>
 		</v-card>
@@ -268,5 +276,7 @@ export default {
 .white-text{
 	color: white;
 }
-
+.v-label {
+    font-size: 0.8rem;
+}
 </style>
